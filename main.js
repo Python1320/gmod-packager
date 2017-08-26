25dcf804dae368cc4baca732bfe38defc35d917c
python1320 <python1320@users.noreply.github.com>
2017-08-27 01:23:21 +0300

    merge directories to dist folder

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
 #!/usr/bin/env node
 
-console.log("TODO");
+console.log("Running GMod Packager...");
+
+var MergeTrees = require('merge-trees');
+ 
+var sources = [];
+
+const node_modules = 'node_modules/';
+const fs = require('fs');
+
+fs.readdirSync(node_modules).forEach(file => {
+	var path_string = "node_modules/" + file;
+	var path_lua = path_string + "/lua";
+
+try {
+if (!fs.lstatSync(path_string).isDirectory() || !fs.lstatSync(path_lua).isDirectory()) {
+return;
+}
+}catch(e){
+if(e.code == 'ENOENT'){
+return;
+}else {
+throw(e);
+}
+}
+	sources.push(path);
+	console.log(" \tAdding: " + path);
+})
+
+var mergeTrees = new MergeTrees(
+  sources,
+  'dist',
+  { overwrite: true });
+ 
+mergeTrees.merge()
