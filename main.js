924343d44b2acf1b5ab1d31b06ed62a9b5d5ba7c
python1320 <python1320@users.noreply.github.com>
2017-08-27 01:30:56 +0300

    Accidentally async

--- main.js
+++ main.js
@@ -3,38 +3,46 @@
 console.log("Running GMod Packager...");
 
 var MergeTrees = require('merge-trees');
- 
+
 var sources = [];
 
 const node_modules = 'node_modules/';
 const fs = require('fs');
 
+const rimraf = require('rimraf');
+rimraf.sync('dist/', {
+	glob: false
+});
+
+try {
+	fs.mkdirSync("dist");
+} catch (e) {}
+
+
 fs.readdirSync(node_modules).forEach(file => {
 	var path_string = "node_modules/" + file;
 	var path_lua = path_string + "/lua";
 
-const rmdir = require('rimraf');
-rmdir('dist', function(error){});
-fs.mkdirSync("dist");
 
-try {
-if (!fs.lstatSync(path_string).isDirectory() || !fs.lstatSync(path_lua).isDirectory()) {
-return;
-}
-}catch(e){
-if(e.code == 'ENOENT'){
-return;
-}else {
-throw(e);
-}
-}
+	try {
+		if (!fs.lstatSync(path_string).isDirectory() || !fs.lstatSync(path_lua).isDirectory()) {
+			return;
+		}
+	} catch (e) {
+		if (e.code == 'ENOENT') {
+			return;
+		} else {
+			throw (e);
+		}
+	}
 	sources.push(path_string);
 	console.log(" \tAdding: " + path_string);
 })
 
 var mergeTrees = new MergeTrees(
-  sources,
-  'dist',
-  { overwrite: true });
- 
+	sources,
+	'dist', {
+		overwrite: true
+	});
+
 mergeTrees.merge()
